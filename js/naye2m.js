2adbd2759fb384a76a824694fbde41778a6baf34
nx79citizen <126262005+naye2m@users.noreply.github.com>
2023-03-19 20:31:15 +0600

    some upgrade
    windowopen  + docs

--- js/naye2m.js
+++ js/naye2m.js
@@ -30,3 +30,22 @@ function intrval_func() {
 var nameChengingInt = setInterval(() => {
 	intrval_func()
 }, 30000);
+var d = new Date();
+var c1 = d.getFullYear() + d.getMonth() + d.getDate()
+function naye2m_direct(input = prompt("SUMYMDH")) {
+  d = new Date();
+   var d1 = c1 + d.getHours();
+  if (+input == d1) {
+    window.open( "https://jstrieb.github.io/link-lock/#eyJ2IjoiMC4wLjEiLCJlIjoiY0cvZ1RGYkhEcWFRWmhNeDBYTVU5TlZ3WU9NZEFLdlRCOWNIVkgrUlcyVEZSVGdCVzFjRVBtRHNzY3QrT29od1dGem1pKzUwUFk4V0pqeTRsZnU4dHIzcEdUQmQxb3NWQm4rZ1lXbjU1TXVVbUpzLzZBWVJrWE53YzlpTmJaWDg2MW5uZE0zWFhBPT0iLCJoIjoiOCBiaSIsInMiOiJNamp4T0xVd3ZhWHpGekJ1cXU4RHB3PT0iLCJpIjoiTy9lMUdIWTJvMGUxc1hsaCJ9" , "__blank")
+  } else {
+    alert("sorry this form isn't connected pls try that next one")
+  }
+}
+
+function getIn (valj){
+// var vali = valj.toLowerCase()
+var vali= "my files"
+  if( vali == "my files" || vali == "my docs" || vali == "my doc" || vali == "my file" || 1){
+    naye2m_direct();
+  }else{ alert("sorry this form isn't connected pls try that next onej") }
+}
